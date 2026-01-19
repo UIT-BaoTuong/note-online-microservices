af76ea03fc203645d7bd880ccfa604f5f19e6dee
BaoTuong <tuongndb>
2026-01-19 23:02:19 +0700

    fix(api.js): update API URLs to use public IP addresses for user and note services fix(init-db.sh): improve database user creation script for clarity and consistency

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
-const API_URL = 'http://localhost:8081/api/users';
-const NOTE_API_URL = 'http://localhost:8082/api/notes';
+const API_URL = 'http://4.240.103.27:8081/api/users';
+const NOTE_API_URL = 'http://4.240.103.27:8082/api/notes';
 
 export async function login(usernameOrEmail, password) {
   const res = await fetch(`${API_URL}/login`, {
--- postgres/init-db.sh
+++ postgres/init-db.sh
@@ -2,14 +2,19 @@
 set -e
 
 function create_user_and_database() {
-	local database=$1
-	echo "  Creating user and database '$database'"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
-	    CREATE USER $database WITH PASSWORD 'Aa123456';
-	    CREATE DATABASE $database;
-			GRANT ALL ON SCHEMA public TO $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+  local database=$1
+  echo "  Creating user and database '$database'"
+  
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
+      CREATE USER $database WITH PASSWORD 'Aa123456';
+      CREATE DATABASE $database OWNER $database; 
+EOSQL
+
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$database" <<-EOSQL
+      GRANT ALL ON SCHEMA public TO $database;
+      ALTER SCHEMA public OWNER TO $database;
 EOSQL
 }
+
 create_user_and_database "note_service"
 create_user_and_database "user_service"
