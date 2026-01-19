6c57e2ac3494fc9d58fa18023085e908de8aae1f
BaoTuong <tuongndb>
2026-01-20 00:30:09 +0700

    fix(postgres): fix permission

--- postgres/init-db.sh
+++ postgres/init-db.sh
@@ -13,6 +13,10 @@ EOSQL
   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$database" <<-EOSQL
       GRANT ALL ON SCHEMA public TO $database;
       ALTER SCHEMA public OWNER TO $database;
+      ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO $database;
+      ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO $database;
+      GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $database;
+      GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $database;
 EOSQL
 }
 
